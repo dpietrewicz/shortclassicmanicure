c96139937ade96097a0efbd34e4c3de84313ea6d
Damian Pietrewicz <damiansmacbook@myMac.local>
2024-10-25 16:34:01 +0200

    navbar

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
 "use strict";
 
-const header = document.querySelector(".header");
-const nav = document.querySelector(".nav");
-const navHeight = nav.getBoundingClientRect().height;
+// const header = document.querySelector(".header");
+// const nav = document.querySelector(".nav");
+// const navHeight = nav.getBoundingClientRect().height;
 
-const stickyNav = function (entries) {
-    const [entry] = entries;
-    // console.log(entry);
+// const stickyNav = function (entries) {
+//     const [entry] = entries;
+//     // console.log(entry);
 
-    if (!entry.isIntersecting) nav.classList.add("sticky");
-    else nav.classList.remove("sticky");
-};
+//     if (!entry.isIntersecting) nav.classList.add("sticky");
+//     else nav.classList.remove("sticky");
+// };
 
-const headerObserver = new IntersectionObserver(stickyNav, {
-    root: null,
-    rootMargin: `-${navHeight}px`,
-    threshold: 0,
-});
-headerObserver.observe(header);
+// const headerObserver = new IntersectionObserver(stickyNav, {
+//     root: null,
+//     rootMargin: `-${navHeight}px`,
+//     threshold: 0,
+// });
+// headerObserver.observe(header);
+
+
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navBtn = document.querySelector(".nav-btn")
+
+hamburger.addEventListener('click', function() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    navBtn.classList.toggle("active");
+})
+
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener('click', () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+    navBtn.classList.remove("active");
+}))
